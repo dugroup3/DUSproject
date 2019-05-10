671e3d5ba6828361b6d42816ff5bb20e517d89a2
Tingran Bian <btrzoe@163.com>
2019-05-10 12:10:52 +0100

    no message

--- Web_Basic_User/Sent_email_FPW.php
+++ Web_Basic_User/Sent_email_FPW.php
@@ -53,4 +53,20 @@ try {
 }
 
 //echo "<script>alert('We have sent you an email');location.href='ResetPassword.php';</script>";
+//##########################################
+$smtpserver = "smtp.163.com";//SMTP Server
+$smtpserverport = 25;//SMTP Server port
+$smtpusermail = "dugroup3@163.com";//SMTP email address
+$smtpemailto = $email;//Email to who
+$smtpuser = "dugroup3";//SMTP Server account
+$smtppass = "jcyhlz666";//SMTP Server Password
+$mailsubject = "Password Recovery";//Email Subject
+$mailbody = "<h1>Your code is $res</h1>";//Email body
+$mailtype = "HTML";//邮件格式（HTML/TXT）,TXT为文本邮件
+##########################################
+$smtp = new smtp($smtpserver, $smtpserverport, true, $smtpuser, $smtppass);
+$smtp->debug = true;//debug information
+$smtp->sendmail($smtpemailto, $smtpusermail, $mailsubject, $mailbody, $mailtype);
+echo "<script>alert('We have sent you an email');location.href='ResetPassword.php';</script>";
+
 ?>
