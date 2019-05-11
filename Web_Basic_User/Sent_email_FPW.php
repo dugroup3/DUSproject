2e27458d0eba1079a4d53a013d6bfd11e93d83c9
chikinggong <604722853@qq.com>
2019-05-11 12:42:35 +0100

    Chiying Jiang 11/05/2019

--- Web_Basic_User/Sent_email_FPW.php
+++ Web_Basic_User/Sent_email_FPW.php
@@ -25,26 +25,28 @@ $_SESSION['email']=$email;
 
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
-    //服务器配置
-    $mail->CharSet ="UTF-8";                     //设定邮件编码
-    $mail->SMTPDebug = 0;                        // 调试模式输出
-    $mail->isSMTP();                             // 使用SMTP
-    $mail->Host = 'smtp.163.com';                // SMTP服务器
-    $mail->SMTPAuth = true;                      // 允许 SMTP 认证
-    $mail->Username = 'dugroup3';                // SMTP 用户名  即邮箱的用户名
-    $mail->Password = 'jcyhlz666';             // SMTP 密码  部分邮箱是授权码(例如163邮箱)
-    $mail->SMTPSecure = 'ssl';                    // 允许 TLS 或者ssl协议
-    $mail->Port = 465;                            // 服务器端口 25 或者465 具体要看邮箱服务器支持
+    //Server configure
+    $mail->CharSet ="UTF-8";                     //Set Email compile code
 
-    $mail->setFrom('dugroup3@163.com', 'Team3');  //发件人
-    $mail->addAddress("$email", 'Customer');  // 收件人//$mail->addAddress('ellen@example.com');  // 可添加多个收件人
-    $mail->addReplyTo("$email", 'info'); //回复的时候回复给哪个邮箱 建议和发件人一致
+    $mail->isSMTP();
+    $mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
+    $mail->SMTPAuth = true; // authentication enabled
+    $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
+    $mail->Host = "tls://smtp.gmail.com";
+    $mail->Port = 587; // or 587
+    $mail->IsHTML(true);
+    $mail->Username = "a604722853@gmail.com";
+    $mail->Password = "jrhopaunxnzeerha";
+
+    $mail->setFrom('a604722853@gmail.com', 'Team3');  //Who send Email
+    $mail->addAddress("$email", 'Customer');  //  Add address
+    $mail->addReplyTo("$email", 'info'); // who repley to
 
     //Content
-    $mail->isHTML(true);                                  // 是否以HTML文档格式发送  发送后客户端可直接显示对应HTML内容
+    $mail->isHTML(true);
     $mail->Subject = 'Password Recovery' . time();
     $mail->Body    = "<p><strong>Your code is $res</strong></p>" . date('Y-m-d H:i:s');
-    $mail->AltBody = '如果邮件客户端不支持HTML则显示此内容';
+    $mail->AltBody = '<p><strong>Your code is $res</strong></p>';
 
     $mail->send();
     echo "<script>alert('We have sent you an email');location.href='ResetPassword.php';</script>";
