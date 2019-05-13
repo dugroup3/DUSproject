5cec5a2da1792d27c7960e34ce7b700f8b345460
chikinggong <604722853@qq.com>
2019-05-13 16:12:28 +0100

    Chiying Jiang 13/05/2019

--- Web_Basic_User/index.php
+++ Web_Basic_User/index.php
@@ -6,13 +6,45 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Durham University Sport</title>
 
+    <link href='../public/lib/fullcalendar-4.1.0/packages/core/main.css' rel='stylesheet'/>
+    <link href='../public/lib/fullcalendar-4.1.0/packages/daygrid/main.css' rel='stylesheet'/>
+    <link href='../public/lib/fullcalendar-4.1.0/packages/timegrid/main.css' rel='stylesheet'/>
+    <script src='../public/lib/fullcalendar-4.1.0/packages/core/main.js'></script>
+    <script src='../public/lib/fullcalendar-4.1.0/packages/interaction/main.js'></script>
+    <script src='../public/lib/fullcalendar-4.1.0/packages/daygrid/main.js'></script>
+    <script src='../public/lib/fullcalendar-4.1.0/packages/timegrid/main.js'></script>
+
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
           integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <!-- Our Custom CSS -->
-    <link href="../public/css/style.css" rel="stylesheet">
+    <link href="../public/css/mainpageCSS.css" rel="stylesheet">
     <!--JS-->
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script>
+
+        document.addEventListener('DOMContentLoaded', function () {
+            var calendarEl = document.getElementById('calendar');
+
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                plugins: ['interaction', 'dayGrid', 'timeGrid'],
+                header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                },
+                events: 'loadCalendar.php',
+                navLinks: true, // can click day/week names to navigate views
+                selectable: true,
+                selectMirror: true,
+                editable: true,
+                eventLimit: true, // allow "more" link when too many events
+            });
+
+            calendar.render();
+        });
+    </script>
 </head>
 <body>
 <!-- Page Content  -->
@@ -30,38 +62,7 @@
                 </button>
             </div>
         </nav>
-        <h2>Main page</h2>
-        <p>Welcome to DUS</p>
-        <table>
-            <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Name</th>
-            </tr>
-            </thead>
-            <tbody>
-            <!--     Test Database connection   -->
-            <?php
-            require_once '../DataBase/database.php';
-            try {
-                $rows = getStaffList();
-                $data_count = count($rows);
-                for ($i = 0; $i < $data_count; $i++) {
-
-                    $ID = $rows[$i]['id'];
-                    $Name = $rows[$i]['name'];
-                    echo "<tr>
-                    <td data-label='ID'>$ID</td>
-                    <td data-label='Name'> $Name</td>
-                    </tr>";
-
-                }
-            } catch (PDOException $e) {
-                die ("Error!: " . $e->getMessage() . "<br/>");
-            }
-            ?>
-            </tbody>
-        </table>
+        <div id='calendar'></div>
     </div>
 </div>
 </body>
---
+++ Web_Basic_User/loadCalendar.php
@@ -0,0 +1,68 @@
+<?php
+/*/**
+ * Created by PhpStorm.
+ * User: jiangchiying
+ * Date: 2019-05-10
+ * Time: 22:38
+ */
+
+require_once '../DataBase/database.php';
+
+$dbh = connectDBPDO();
+//Get the booking from database
+$data = array();
+$query = "SELECT * FROM `Booking` as B LEFT JOIN Facility as F ON B.FacilityID = F.FacilityID WHERE B.Totalcost!=0";
+$statement = $dbh->prepare($query);
+$statement->execute();
+$result = $statement->fetchAll();
+
+//Get the event from database
+//$data2 = array();
+$query2 = "SELECT * FROM `Event` as E LEFT JOIN Booking as B ON E.BookingID = B.BookingID";
+$statement2 = $dbh->prepare($query2);
+$statement2->execute();
+$result2 = $statement2->fetchAll();
+
+foreach ($result as $row) {
+    if($row["FacilityID"]==14){
+        $row['Color']="yellow";
+    }
+    if($row["FacilityID"]==15){
+        $row['Color']="Green";
+    }
+    if($row["FacilityID"]==16){
+        $row['Color']="#e6e6fa";
+    }
+    if($row["FacilityID"]==17){
+        $row['Color']="#fff0f5";
+    }
+
+    $data[] = array(
+        'id' => $row["BookingID"],
+        'title' => $row["Name"],
+        'start' => $row["Starttime"],
+        'end' => $row["Endtime"],
+        'color'=>$row['Color'],
+
+    );
+}
+
+foreach ($result2 as $row) {
+    $dayofweek=$row['DaysOfWeek'];
+    explode(',',$dayofweek,0);
+    $data[] = array(
+        'id' => $row['EventID'],
+        'title' => $row['Eventname'],
+        'daysOfWeek' => $dayofweek,
+        'startTime' =>$row['EventStartTime'],
+        'endTime'=>$row['EventEndTime'],
+        'startRecur'=>$row['Starttime'],
+        'endRecur'=>$row['Endtime'],
+        'color'=>'red',
+
+    );
+}
+
+
+echo json_encode($data);
+?>
